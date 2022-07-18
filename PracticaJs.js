fb20004f0c8ec2a148d1e82a8f5d2c0d652b8ac2
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2022-07-18 15:51:07 -0300

    :point_up: first commit

---
+++ PracticaJs.js
@@ -0,0 +1,13 @@
+const name = "Fernando"
+const age = 32
+const isMarried = false
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const compas = [Rolo, Javi, Mel, Ezequiel, Max]
+
+const Object = {
+    name: "Fernando",
+    age: 32,
+    isMarried: false,
+    numberArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    compas: [Rolo, Javi, Mel, Ezequiel, Max]
+}
