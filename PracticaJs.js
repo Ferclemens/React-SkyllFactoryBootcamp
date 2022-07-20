94dcb9b5c3b9852921840b8f95ddcf14235f11e7
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2022-07-20 10:26:53 -0300

    fix: String Array!

--- PracticaJs.js
+++ PracticaJs.js
@@ -2,12 +2,16 @@ const name = "Fernando"
 const age = 32
 const isMarried = false
 const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const compas = [Rolo, Javi, Mel, Ezequiel, Max]
+const compas = ["Rolo", "Javi", "Mel", "Ezequiel", "Max"]
 
-const Object = {
+const object = {
     name: "Fernando",
     age: 32,
     isMarried: false,
     numberArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    compas: [Rolo, Javi, Mel, Ezequiel, Max]
+    compas: ["Rolo", "Javi", "Mel", "Ezequiel", "Max"]
 }
+
+console.log(numberArray);
+console.log(compas);
+console.log(object);
